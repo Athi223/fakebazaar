2dc6240a8f8c2bd606dc87b3d071b5326c31c717
Atharv Phadnis <athiphadnis@gmail.com>
2022-08-14 01:09:31 +0530

    Updated App Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { useEffect, useState } from 'react'
+import { Routes, Route } from "react-router-dom"
+import { AuthContext } from './Contexts/AuthContext'
+import { BootstrapContext } from './Contexts/BootstrapContext'
+import { StoreContext } from './Contexts/StoreContext'
+import bootstrap from 'bootstrap/dist/js/bootstrap'
+import Layout from './Components/Layout'
+import Home from './Components/Home'
+import Categories from './Components/Categories'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+	const baseURL = "https://fakestoreapi.com"
+	const [ authToken, setAuthToken ] = useState("")
+	const [ products, setProducts ] = useState([])
+	const [ categories, setCategories ] = useState([])
+	const [ cart, setCart ] = useState([])
+	const viewProduct = (product) => console.log(product)
+
+	useEffect(() => {
+		fetch(`${baseURL}/products`)
+		.then(response => response.json())
+		.then(json => setProducts(json))
+		.catch(error => console.log(error))
+
+		fetch(`${baseURL}/products/categories`)
+		.then(response => response.json())
+		.then(json => setCategories(json))
+		.catch(error => console.log(error))
+
+		const toastElList = document.querySelectorAll('.toast')
+		toastElList.forEach(toastEl => new bootstrap.Toast(toastEl, {}))
+	}, [ baseURL ])
+
+	return (
+		<AuthContext.Provider value={{ authToken, setAuthToken, baseURL }}>
+			<BootstrapContext.Provider value={{ bootstrap }}>
+				<StoreContext.Provider value={{ products, categories, viewProduct, cart, setCart }}>
+					<Routes>
+						<Route path="/" element={<Layout />}>
+							<Route index element={<Home />} />
+							<Route path="categories" element={<Categories />} />
+						</Route>
+					</Routes>
+				</StoreContext.Provider>
+			</BootstrapContext.Provider>
+		</AuthContext.Provider>
+	)
 }
-
-export default App;
