0faec3352db369c7bfeccdb35a5905b52efbafbf
Atharv Phadnis <athiphadnis@gmail.com>
2022-08-15 22:38:59 +0530

    Prettified with Prettifier

--- src/Components/Authenticate.js
+++ src/Components/Authenticate.js
@@ -5,51 +5,77 @@ import { BootstrapContext } from "../Contexts/BootstrapContext"
 export default function Authenticate() {
 	const { baseURL, setAuthToken } = useContext(AuthContext)
 	const { bootstrap } = useContext(BootstrapContext)
-	const [ username, setUsername ] = useState("")
-	const [ password, setPassword ] = useState("")
-	const [ error, setError ] = useState(false)
-	const authenticate = (e) => {
+	const [username, setUsername] = useState("")
+	const [password, setPassword] = useState("")
+	const [error, setError] = useState(false)
+
+	const authenticate = e => {
 		e.preventDefault()
 		fetch(`${baseURL}/auth/login`, {
 			method: "POST",
 			headers: {
-				"Content-Type": "application/json"
+				"Content-Type": "application/json",
 			},
-			body: JSON.stringify({ username: username, password: password })
+			body: JSON.stringify({ username: username, password: password }),
 		})
-		.then(response => response.ok ? response.json() : Promise.reject(response))
-		.then(json => {
-			setAuthToken(json.token)
-			const authenticateModal = bootstrap.Modal.getInstance("#authenticateModal")
-			authenticateModal.hide()
-		})
-		.catch(() => setError(true))
+			.then(response => (response.ok ? response.json() : Promise.reject(response)))
+			.then(json => {
+				setAuthToken(json.token)
+				const authenticateModal = bootstrap.Modal.getInstance("#authenticateModal")
+				authenticateModal.hide()
+			})
+			.catch(() => setError(true))
 	}
 	return (
-		<div className="modal fade" id="authenticateModal" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="authenticateModalLabel" aria-hidden="true">
+		<div
+			className="modal fade"
+			id="authenticateModal"
+			data-bs-backdrop="static"
+			data-bs-keyboard="false"
+			tabIndex="-1"
+			aria-labelledby="authenticateModalLabel"
+			aria-hidden="true">
 			<div className="modal-dialog modal-dialog-centered">
 				<div className="modal-content">
 					<div className="modal-header">
-						<h5 className="modal-title" id="authenticateModalLabel">Sign In to your Account</h5>
+						<h5 className="modal-title" id="authenticateModalLabel">
+							Sign In to your Account
+						</h5>
 						<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 					</div>
 					<div className="modal-body">
 						<form onSubmit={authenticate}>
 							<div className="mb-3">
-								<label htmlFor="username" className="form-label">Username</label>
-								<input value={username} onChange={e => setUsername(e.target.value)} type="text" className="form-control" id="username" required />
+								<label htmlFor="username" className="form-label">
+									Username
+								</label>
+								<input
+									value={username}
+									onChange={e => setUsername(e.target.value)}
+									type="text"
+									className="form-control"
+									id="username"
+									required
+								/>
 							</div>
 							<div className="mb-3">
-								<label htmlFor="password" className="form-label">Password</label>
-								<input value={password} onChange={e => setPassword(e.target.value)} type="password" className="form-control" id="password" required />
-							</div>
-							<div className="mb-3 form-check">
-								<input type="checkbox" className="form-check-input" id="rememberme" />
-								<label className="form-check-label" htmlFor="rememberme">Remember Me</label>
+								<label htmlFor="password" className="form-label">
+									Password
+								</label>
+								<input
+									value={password}
+									onChange={e => setPassword(e.target.value)}
+									type="password"
+									className="form-control"
+									id="password"
+									required
+								/>
 							</div>
 							{error && <div className="mb-3 text-center text-danger">Invalid Username/Password</div>}
-							<div className='text-center'>
-								<button type="submit" className="btn btn-primary">Sign In</button>
+							<div className="text-center">
+								<button type="submit" className="btn btn-primary">
+									Sign In
+								</button>
 							</div>
 						</form>
 					</div>
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,10 +1,11 @@
 import { useContext } from "react"
 import { NavLink, Outlet } from "react-router-dom"
 import { AuthContext } from "../Contexts/AuthContext"
-import Cart from './Cart'
-import Authenticate from './Authenticate'
-import Notification from './Notifications'
-import { LogIn, ShoppingCart, User } from 'react-feather'
+import Cart from "./Cart"
+import Authenticate from "./Authenticate"
+import Notification from "./Notifications"
+import { LogIn, ShoppingCart, User } from "react-feather"
+import Product from "./Product"
 
 export default function Layout() {
 	const { authToken, setAuthToken } = useContext(AuthContext)
@@ -13,42 +14,77 @@ export default function Layout() {
 		<div>
 			<nav className="navbar navbar-expand-lg navbar-light bg-navbar">
 				<div className="container-fluid">
-					<a className="navbar-brand" href="/">FakeBazaar</a>
-					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+					<a className="navbar-brand" href="/">
+						FakeBazaar
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarContent"
+						aria-controls="navbarContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon"></span>
 					</button>
 					<div className="collapse navbar-collapse" id="navbarContent">
 						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 							<li className="nav-item">
-								<NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
+								<NavLink className="nav-link" aria-current="page" to="/">
+									Home
+								</NavLink>
 							</li>
 							<li className="nav-item">
-								<NavLink className="nav-link" to="/categories">Categories</NavLink>
+								<NavLink className="nav-link" to="/categories">
+									Categories
+								</NavLink>
 							</li>
+							{authToken && (
+								<li className="nav-item">
+									<NavLink className="nav-link" to="/checkout">
+										Checkout
+									</NavLink>
+								</li>
+							)}
 						</ul>
 						<div className="btn-group" role="group">
-							<button className='btn'>
-								<ShoppingCart size={20} data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" />
+							<button className="btn">
+								<ShoppingCart
+									size={20}
+									data-bs-toggle="offcanvas"
+									data-bs-target="#Cart"
+									aria-controls="Cart"
+								/>
 							</button>
-							{authToken ? 
+							{authToken ? (
 								<div className="dropdown">
-									<button className="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+									<button
+										className="btn dropdown-toggle"
+										type="button"
+										data-bs-toggle="dropdown"
+										aria-expanded="false">
 										<User size={20} />
 									</button>
 									<ul className="dropdown-menu dropdown-menu-end">
-										<li><button className="dropdown-item" onClick={() => setAuthToken("")}>Logout</button></li>
+										<li>
+											<button className="dropdown-item" onClick={() => setAuthToken("")}>
+												Logout
+											</button>
+										</li>
 									</ul>
 								</div>
-								: <button className='btn' data-bs-toggle="modal" data-bs-target="#authenticateModal">
+							) : (
+								<button className="btn" data-bs-toggle="modal" data-bs-target="#authenticateModal">
 									<LogIn size={20} />
 								</button>
-							}
+							)}
 						</div>
 					</div>
 				</div>
 			</nav>
 			<Outlet />
 			<Authenticate />
+			<Product />
 			<Cart />
 			<Notification />
 		</div>
