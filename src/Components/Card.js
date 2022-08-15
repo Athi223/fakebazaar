da848aa9ebc2d7ea4dee27415f94c2d3a2609ae8
Atharv Phadnis <athiphadnis@gmail.com>
2022-08-15 23:07:32 +0530

    Fixed Btton click triggering Div click

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,26 +6,28 @@ export default function Card({ product }) {
 	const { bootstrap } = useContext(BootstrapContext)
 	const { setCart, setSelectedProduct } = useContext(StoreContext)
 
-	const addToCart = id => {
+	const addToCart = (e, id) => {
+		e.stopPropagation()
 		const addedToCart = bootstrap.Toast.getInstance("#addedToCart")
 		addedToCart.show()
 		setCart(cart => [...cart, id])
 	}
 
+	const showProduct = () => {
+		const productModal = new bootstrap.Modal("#productModal")
+		productModal.show()
+		setSelectedProduct(product)
+	}
+
 	return (
-		<div
-			className="card border-secondary shadow h-100"
-			role="button"
-			data-bs-toggle="modal"
-			data-bs-target="#productModal"
-			onClick={() => setSelectedProduct(product)}>
+		<div className="card border-secondary shadow h-100" role="button" onClick={showProduct}>
 			<img src={product.image} className="card-img-top p-3" height={300} alt={product.title} />
 			<div className="card-body">
 				<h4 className="card-title">{product.title}</h4>
 			</div>
 			<div className="card-footer d-flex justify-content-between">
 				<h4 className="card-text">${product.price}</h4>
-				<button className="btn btn-sm btn-primary m-0" onClick={() => addToCart(product.id - 1)}>
+				<button className="btn btn-sm btn-primary m-0" onClick={e => addToCart(e, product.id - 1)}>
 					Add to Cart
 				</button>
 			</div>
