58d2e1fbcd0b9d529ce541addfebf8c85846ba09
Atharv Phadnis <athiphadnis@gmail.com>
2022-08-21 12:38:03 +0530

    Moved firebase config to separate file

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyCiTm4GF9uFclQKhW20PS_t3BHN8knBIp8",
+	authDomain: "fakebazaar-42069.firebaseapp.com",
+	databaseURL: "https://fakebazaar-42069-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "fakebazaar-42069",
+	storageBucket: "fakebazaar-42069.appspot.com",
+	messagingSenderId: "410714308136",
+	appId: "1:410714308136:web:0257bfcb6ca1926ec83f1d",
+}
