d984d5bdeeca5c0d2a920a8336a1a46cd981722c
Atharv Phadnis <athiphadnis@gmail.com>
2022-08-24 00:06:45 +0530

    Categories dropdown instead of tabs

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,59 +1,43 @@
-import { useContext, useEffect, useState } from "react"
+import { Link, Navigate, useParams } from "react-router-dom"
+import { useContext } from "react"
 import { StoreContext } from "../Contexts/StoreContext"
 import Card from "./Card"
 
 export default function Categories() {
-	const { products, categories } = useContext(StoreContext)
-	const [selectedCategory, setSelectedCategory] = useState("")
+	const { category } = useParams()
+	const { categories, products } = useContext(StoreContext)
 
-	useEffect(() => {
-		setSelectedCategory(categories[0])
-	}, [categories])
-
-	return (
-		<div className="mt-1">
-			<ul className="nav nav-tabs" id="myTab" role="tablist">
+	return category ? (
+		categories.includes(category) ? (
+			<div className="m-4">
+				<h4>{category}</h4>
+				<hr />
+				<div className="bg-secondary p-4 rounded" style={{ "--bs-bg-opacity": 0.75 }}>
+					<div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-5">
+						{products
+							.filter(product => product.category === category)
+							.map((product, index) => (
+								<div className="col" key={index}>
+									<Card product={product} />
+								</div>
+							))}
+					</div>
+				</div>
+			</div>
+		) : (
+			<Navigate to="/" />
+		)
+	) : (
+		<div className="mt-5 container">
+			<ul className="list-group">
 				{categories.map((category, index) => (
-					<li key={index} className="nav-item" role="presentation">
-						<button
-							className={"text-capitalize nav-link " + (selectedCategory === category ? "active" : "")}
-							id={category.replaceAll(" ", "").replaceAll("&", "").replaceAll(",", "") + "-tab"}
-							data-bs-toggle="tab"
-							data-bs-target={
-								"#" + category.replaceAll(" ", "").replaceAll("&", "").replaceAll(",", "") + "-tab-pane"
-							}
-							type="button"
-							role="tab"
-							aria-controls={
-								category.replaceAll(" ", "").replaceAll("&", "").replaceAll(",", "") + "-tab-pane"
-							}
-							aria-selected={selectedCategory === category ? "true" : "false"}>
+					<li className="list-group-item" key={index}>
+						<Link className="text-decoration-none" to={category}>
 							{category}
-						</button>
+						</Link>
 					</li>
 				))}
 			</ul>
-			<div className="tab-content p-5" id="myTabContent">
-				{categories.map((category, catIndex) => (
-					<div
-						key={catIndex}
-						className={"tab-pane fade " + (selectedCategory === category ? "show active" : "")}
-						id={category.replaceAll(" ", "").replaceAll("&", "").replaceAll(",", "") + "-tab-pane"}
-						role="tabpanel"
-						aria-labelledby={category.replaceAll(" ", "").replaceAll("&", "").replaceAll(",", "") + "-tab"}
-						tabIndex="0">
-						<div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-5">
-							{products
-								.filter(product => product.category === category)
-								.map((product, index) => (
-									<div className="col" key={index}>
-										<Card product={product} />
-									</div>
-								))}
-						</div>
-					</div>
-				))}
-			</div>
 		</div>
 	)
 }
